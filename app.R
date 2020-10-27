82b84a82f02d0aba05e5f6036118e9a387c09778
Flemming Albers <fl-albers@web.de>
2020-10-27 16:27:24 +0100

    Vorschau changes

--- app.R
+++ app.R
@@ -876,9 +876,9 @@ server <- function(input, output, session) {
         
         #damit PDF-Ausgabe mit Iframe funktioniert müssen Dateien in den www-Ordner vom Workingdirectory                
         
-        system(str_c("java -jar target/r2q-pdf-gen-0.3.0-jar-with-dependencies.jar ",input$Massnahme,".pdf ", as.character(subset(list_Massnahmen, Massnahmen == input$Massnahme)[1,2])))
+        system(paste0("java -jar target/r2q-pdf-gen-0.3.0-jar-with-dependencies.jar ",input$Massnahme,".pdf ", as.character(subset(list_Massnahmen, Massnahmen == input$Massnahme)[1,2])))
         
-        file.copy(str_c("/home/user/R2QPdfGen/",input$Massnahme,".pdf"), str_c("./www/",input$Massnahme,".pdf"))
+        file.copy(str_c("/home/user/",input$Massnahme,".pdf"), str_c("./www/",input$Massnahme,".pdf"))
         
         output$vorschaupdf <- renderText({
             return(paste('<iframe style="height:600px; width:100%" src="', str_c(input$Massnahme,".pdf"), '"></iframe>', sep = ""))
