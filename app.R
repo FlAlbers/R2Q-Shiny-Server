44fe379a5c6f3a107bea5d37de2d64ca08f11507
Flemming Albers <fl-albers@web.de>
2020-10-27 20:26:00 +0100

    namePDF fix

--- app.R
+++ app.R
@@ -888,7 +888,7 @@ server <- function(input, output, session) {
         syscommand <- paste0("java -jar /home/shiny/R2QPdfGen/target/r2q-pdf-gen-0.3.0-jar-with-dependencies.jar ",pdfOutPath,namePDF,".pdf ", as.character(subset(list_Massnahmen, Massnahmen == input$Massnahme)[1,2]))
         
         system(syscommand)
-        file.copy(str_c(pdfOutPath,input$Massnahme,".pdf"), str_c("./www/",namePDF,".pdf"),overwrite = TRUE)
+        file.copy(str_c(pdfOutPath,namePDF,".pdf"), str_c("./www/",namePDF,".pdf"),overwrite = TRUE)
         
         output$vorschaupdf <- renderText({
             return(paste('<iframe style="height:600px; width:100%" src="', str_c(namePDF,".pdf"), '"></iframe>', sep = ""))
